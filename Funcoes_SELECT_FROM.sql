d44209206b9934103d935522fa61861990d6de6b
Yuri Marques Barboza <63083579+Yuri-m-b@users.noreply.github.com>
2023-02-08 16:12:54 -0300

    Add files via upload

---
+++ Funcoes_SELECT_FROM.sql
@@ -0,0 +1,11 @@
+-- Seleciona todas as colunas da tabela person.Person
+SELECT *
+FROM person.Person;
+
+-- Seleciona a coluna Title da tabela person.Person
+SELECT FirstName, MiddleName, LastName 
+FROM person.Person; 
+
+-- Seleciona todas as colunas da tabela person.EmailAddress
+SELECT *
+FROM person.EmailAddress;
